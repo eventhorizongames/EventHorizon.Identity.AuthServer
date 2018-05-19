846c20780529763fe8776ed8a146113067966d18
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2018-05-18 19:49:09 -0500

    Update init db scripts.

--- _scripts/initDB.sh
+++ _scripts/initDB.sh
@@ -1,1 +1,5 @@
-docker run -v $PWD/appsettings.json:/app/appsettings.Production.json -e ASPNETCORE_ENVIRONMENT=Production --entrypoint dotnet ehz/identity/authserver EventHorizon.Identity.AuthServer.dll /initdb
+docker run -v ~/Configs/Identity/appsettings.Production.json:/app/appsettings.Production.json \
+-v ~/Configs/Identity/admins.Production.json:/app/admins.Production.json \
+-v ~/EventHorizon.Keys/AuthServer/certificate.pfx:/app/certificate.pfx \
+-e ASPNETCORE_ENVIRONMENT=Production \
+--entrypoint dotnet ehz/identity/authserver EventHorizon.Identity.AuthServer.dll /initdb
