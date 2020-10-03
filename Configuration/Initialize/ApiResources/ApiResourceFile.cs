94e31d2aaf681210a4c41108025235700f61cd6b
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2020-10-02 21:01:09 -0500

    Added Api Resources  Setup from File. Only works for empty API Resources Table. Included default api-resources.json

---
+++ Configuration/Initialize/ApiResources/ApiResourceFile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace EventHorizon.Identity.AuthServer.Configuration.Initialize.ApiResources
+{
+    public class ApiResourceFile
+    {
+        public List<ApiResourceConfiguration> ApiResources { get; set; } = new List<ApiResourceConfiguration>();
+    }
+
+    public class ApiResourceConfiguration
+    {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public List<string> ClaimTypes { get; set; }
+    }
+}
--- Configuration/Initialize/ApiResources/InitializeApiResourcesHandler.cs
+++ Configuration/Initialize/ApiResources/InitializeApiResourcesHandler.cs
@@ -5,17 +5,22 @@ using System.Threading.Tasks;
 using IdentityServer4.EntityFramework.Mappers;
 using IdentityServer4.Models;
 using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace EventHorizon.Identity.AuthServer.Configuration.Initialize.ApiResources
 {
     public struct InitializeApiResourcesHandler : IRequestHandler<InitializeApiResourcesCommand, bool>
     {
-        readonly HistoryExtendedConfigurationDbContext _context;
+        private readonly IHostEnvironment _hostEnvironment;
+        private readonly HistoryExtendedConfigurationDbContext _context;
 
         public InitializeApiResourcesHandler(
+            IHostEnvironment hostEnvironment,
             HistoryExtendedConfigurationDbContext context
         )
         {
+            _hostEnvironment = hostEnvironment;
             _context = context;
         }
 
@@ -33,14 +38,26 @@ namespace EventHorizon.Identity.AuthServer.Configuration.Initialize.ApiResources
                 true
             );
         }
-        private static IEnumerable<ApiResource> GetApiResources()
+
+        private IEnumerable<ApiResource> GetApiResources()
         {
-            // TODO: Load this from a file
-            return new ApiResource[]
-            {
-                new ApiResource("api1", "My API #1"),
-                new ApiResource("roles", "Role", new[] {"role"})
-            };
+            var apiResourceConfiguration = new ConfigurationBuilder()
+                .SetBasePath(_hostEnvironment.ContentRootPath)
+                .AddJsonFile("api-resources.json")
+                .AddJsonFile($"api-resources.{_hostEnvironment.EnvironmentName}.json", true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var apiResourceFile = new ApiResourceFile();
+            apiResourceConfiguration.Bind(
+                apiResourceFile
+            );
+
+            return apiResourceFile.ApiResources.Select(resource => new ApiResource(
+                resource.Name,
+                resource.DisplayName,
+                resource.ClaimTypes
+            ));
         }
     }
 }
