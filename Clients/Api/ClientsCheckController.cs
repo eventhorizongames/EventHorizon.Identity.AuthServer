6e1749b2416958f40996827ef35a4c28a7a36fbb
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2020-10-19 23:27:11 -0500

    Added API to check for existence of Client.

---
+++ Clients/Api/ClientsCheckController.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using EventHorizon.Identity.AuthServer.Clients.Services.Query;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventHorizon.Identity.AuthServer.Clients.Api
+{
+    [ApiController]
+    [Route("api/clients/{clientId}")]
+    [Authorize(Roles = "Admin", AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class ClientsCheckController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ClientsCheckController(
+            IMediator mediator
+        )
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("check")]
+        public async Task<ActionResult<bool>> Create(
+            string clientId
+        )
+        {
+            var result = await _mediator.Send(
+                new QueryForClientById(
+                    clientId
+                )
+            );
+
+            return result.Success;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -5,12 +5,22 @@ Need to make sure AutoHistory is not created during any migrations other than th
 
 ## Create Migration
 
+~~~ bash
 dotnet ef migrations add DotnetUpdate --context ApplicationDbContext --output-dir Migrations\ApplicationDb
 
 dotnet ef migrations add DotnetUpdate --context HistoryExtendedConfigurationDbContext --output-dir Migrations\ConfigurationDb
 
 dotnet ef migrations add DotnetUpdate --context PersistedGrantDbContext --output-dir Migrations\PersistedGrantDb
+~~~
 
 ## Update Connected DB
 
+~~~ bash
 dotnet run /initdb
+~~~
+
+## Create Docker Image
+
+~~~ bash
+docker build -t canhorn/ehz-identity-authserver:dev . 
+~~~
