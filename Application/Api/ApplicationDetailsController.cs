d0d150bce974c83990fbb908135e0d58e04685d8
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2021-03-09 13:28:08 -0600

    Application Version Details Added API controller that can be used to query the Application Details for the Deployment.
    Added Azure Pipelines file, created to match what is configured in Azure DevOps.

---
+++ Application/Api/ApplicationDetailsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace EventHorizon.Identity.AuthServer.Application.Api
+{
+    [ApiController]
+    [Route("api/application")]
+    public class ApplicationDetailsController
+        : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ApplicationDetailsController(
+            IConfiguration configuration
+        )
+        {
+            this._configuration = configuration;
+        }
+
+        [HttpGet("details")]
+        public ActionResult<ApplicationDetailsModel> Details()
+        {
+            return new ApplicationDetailsModel
+            {
+                ApplicationVersion = _configuration["APPLICATION_VERSION"],
+            };
+        }
+
+        public class ApplicationDetailsModel
+        {
+            public string ApplicationVersion { get; set; }
+        }
+    }
+}
--- Dockerfile
+++ Dockerfile
@@ -13,6 +13,9 @@ RUN dotnet publish --output /app/ --configuration Release
 
 # Stage 2
 FROM mcr.microsoft.com/dotnet/core/aspnet:3.1 AS runtime
+ARG BUILD_VERSION=0.0.0
+ENV APPLICATION_VERSION=$BUILD_VERSION
+
 WORKDIR /app
 COPY --from=build /app .
 ENTRYPOINT ["dotnet", "EventHorizon.Identity.AuthServer.dll"]
